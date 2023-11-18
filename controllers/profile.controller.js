76060cc166480dd05d0a2b0c05e3ecade7463c6a
voidwalker1707 <angeldavid3511@gmail.com>
2023-11-17 20:57:22 -0500

    buy

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -2,7 +2,7 @@ import User from "../models/usuario.js";
 const getProfile = async (req,res,next)=>{
     const {userId} = req
     const foundUser= await User.findOne({_id: userId})
-    const newUser = {Nombre:foundUser.Nombre,Correo:foundUser.Correo}
+    const newUser = {Nombre:foundUser.Nombre,Correo:foundUser.Correo,historial_de_compra:foundUser.historial_de_compra}
     res.status(200).json({
         ok:true,
         newUser
